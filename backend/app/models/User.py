8628821d05d5e354a764fab6ac03bfe27b1450ea
PauloS5 <paulosocratesdesouza@gmail.com>
2025-09-28 01:01:31 -0500

    Model user criado [#42]

---
+++ backend/app/models/User.py
@@ -0,0 +1,18 @@
+﻿from sqlmodel import Field, SQLModel
+
+class User(SQLModel, table=True):
+    id: int = Field(
+        default=None,
+        primary_key=True,
+        title="ID",
+        description="Identificador único do usuário.")
+    name: str = Field(
+        title="Nome",
+        description="Nome completo do usuário.")
+    email: str = Field(
+        title="E-mail",
+        description="E-mail do usuário.")
+    pix_key: str = Field(
+        default=None,
+        title="Chave Pix",
+        description="Chave pix do usuário.")
