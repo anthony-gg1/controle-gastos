4730a6b6687ebfbf057b3da99c1904ff37e5444f
PauloS5 <paulosocratesdesouza@gmail.com>
2025-09-28 00:48:57 -0500

    Enum para categoria criado [#39]

---
+++ backend/app/Enums/Category.py
@@ -0,0 +1,15 @@
+﻿from enum import Enum, auto
+
+class CategoryExpense(Enum):
+    OUTRO       = 0
+    ALUGUEL     = auto()
+    ALIMENTAÇÃO = auto()
+    TRANSPORTE  = auto()
+    CONTAS      = auto()
+
+class CategoryRevenue(Enum):
+    OUTRO       = 0
+    SALARIO     = auto()
+    BONUS       = auto()
+    VENDA       = auto()
+    REEMBOLSO   = auto()
