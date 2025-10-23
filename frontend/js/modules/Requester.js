55d44d12d5affb2e4c1784aa8080c4e9fb60f8e6
PauloS5 <paulosocratesdesouza@gmail.com>
2025-10-23 00:06:14 -0500

    Arquivos .js criados

---
+++ frontend/js/modules/Requester.js
@@ -0,0 +1,62 @@
+﻿class User {
+
+    // Métodos para Requisição
+    static getData() {
+        // Puxar dados
+        return {};
+    }
+    static setData(data) {
+        // Puxar definir dados
+    }
+
+}
+
+class Goal {
+
+    // Métodos para buscar registros
+    static findAll() {
+        // Listar registros
+        return [];
+    }
+    static findById(id) {
+        // Buscar registros
+        return {};
+    }
+
+    // Métodos para realizar alterações nos registros
+    static save(data) {
+        // Salvar registro
+    }
+    static update(data) {
+        // Atualizar registro
+    }
+    static delete(id) {
+        // Apagar registro
+    }
+
+}
+
+class Transaction {
+   
+    // Métodos para buscar registros
+    static findAll() {
+        // Listar registros
+        return [];
+    }
+    static findById(id) {
+        // Buscar registros
+        return {};
+    }
+
+    // Métodos para realizar alterações nos registros
+    static save(data) {
+        // Salvar registro
+    }
+    static update(data) {
+        // Atualizar registro
+    }
+    static delete(id) {
+        // Apagar registro
+    }
+
+}
