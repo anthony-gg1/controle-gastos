1dd746e856d03dab3e3f6420a117ce3d180b0f9f
PauloS5 <paulocratesdesouza@gmail.com>
2025-10-31 16:09:25 -0500

    Função para valdiar CPFs criada [#116]

--- frontend/js/Validator.js
+++ frontend/js/Validator.js
@@ -9,4 +9,36 @@
         }
     }
 
+    static validateCpf(value) {
+        // Removendo espaços
+        value = value.trim();
+
+        // Verificando a existencia de pontos e hífens
+        if (!(
+            value.charAt(3) == "." &&
+            value.charAt(7) == "." &&
+            value.charAt(11) == "-"
+        )) {
+            return false;
+        }
+
+        // Removendo caracteres não numéricos
+        value = value.replace(".", "");
+        value = value.replace(".", "");
+        value = value.replace("-", "");
+
+        // Verificando o tamanho
+        if (value.length != 11) {
+            return false
+        }
+
+        // Verificando se há somente números
+        if (!/^[0-9]+$/.test(value)) {
+            return false;
+        }
+
+        return true;
+    }
 }
+
+console.log(Validator.validateCpf("111.000.222-20"));
