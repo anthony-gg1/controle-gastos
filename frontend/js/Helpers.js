c2c4e6b2f949ef9c27b1de35ebf9de1475b12477
PauloS5 <paulosocratesdesouza@gmail.com>
2025-11-01 16:20:42 -0500

    Constantes para os end-poins criada

--- frontend/js/Helpers.js
+++ frontend/js/Helpers.js
@@ -3,3 +3,10 @@ const FAKE_API_URL = "http://localhost/fake-api"
 
 // Endereco da API
 export const API_URL = FAKE_API_URL;
+
+// End-points
+export const END_POINTS = {
+    user: "/user",
+    goals: "/goals",
+    transactions: "/transactions"
+};
