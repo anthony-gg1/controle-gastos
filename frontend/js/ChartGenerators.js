803b4de3666b4b4a7159d0e8d7e5558016c41120
PauloS5 <paulosocratesdesouza@gmail.com>
2025-11-01 21:32:49 -0500

    Gráfico de Pizza da página pricipal criado [#119]

---
+++ frontend/js/ChartGenerators.js
@@ -0,0 +1,44 @@
+﻿export function generateTransactionPieChart(canvasId, revenueCount, expenseCount) {
+    const ctx = document.getElementById(canvasId);
+
+    const data = {
+        labels: ['Receitas', 'Despesas'],
+        datasets: [{
+            label: 'Proporção entre Receitas e Despesas',
+            data: [revenueCount, expenseCount],
+            backgroundcolor: [
+                'rgb(86,255,178)',
+                'rgb(255,95,97)'
+            ],
+            hoverOffset: 10
+        }]
+    };
+
+    const options = {
+            responsive: true,
+            maintainAspectRatio: false,
+            borderWidth: 1.8,
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Receitas x Despesas",
+                    color: "#fff",
+                    font: {
+                        size: 24,
+                        weight: "bold"
+                    }
+                },
+                legend: {
+                    labels: {
+                        color: '#fff' // 🟢 Cor do texto da legenda
+                    }
+                }
+            }
+        }
+
+    const chart = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: options
+    })
+}
