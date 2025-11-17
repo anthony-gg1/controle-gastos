0fc149f3198856c9de1ff68d4ca47096a9ef8db7
PauloS5 <paulosocratesdesouza@gmail.com>
2025-11-17 15:16:57 -0500

    feat: implementa o model de Transação - Ref. #146

--- backend/core/transaction/models.py
+++ backend/core/transaction/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from user.models import User
+from datetime import datetime
 
-# Create your models here.
+class TransactionType(models.TextChoices):
+    REVENUE = 'REV', 'Revenue'
+    SPENT   = 'SPE', 'Spent'
+
+class TransactionCategory(models.TextChoices):
+    INC_SALARY     = 'INC_SALARY',     'Salary' 
+    INC_INVESTMENT = 'INC_INVESTMENT', 'Investment'
+    INC_FREELANCE  = 'INC_FREELANCE',  'Freelance'
+    INC_DONATION   = 'INC_DONATION',   'Donation'
+    INC_OTHER      = 'INC_OTHER',      'Other Income'
+
+    EXP_FOOD           = 'EXP_FOOD',          'Food' 
+    EXP_TRANSPORT      = 'EXP_TRANSPORT',     'Transport'
+    EXP_ENTERTAINMENT  = 'EXP_ENTERTAINMENT', 'Entertainment'
+    EXP_INVOICE        = 'EXP_INVOICE',       'Invoice'
+    EXP_OTHER          = 'EXP_OTHER',         'Other Expense'
+
+class Transaction(models.Model):
+    id = models.BigAutoField(
+        db_column='transaction_id_pk',
+        primary_key=True)
+    
+    user = models.ForeignKey(
+        db_column='transaction_user_id_fk',
+        to=User,
+        on_delete=models.CASCADE,
+        null=False)
+    
+    value = models.DecimalField(
+        db_column='transaction_value',
+        max_digits=10,
+        decimal_places=2,
+        default=0.0,
+        min=0.0,
+        null=False)
+    
+    type = models.CharField(
+        db_column='transaction_type',
+        max_length=3,
+        choices=TransactionType.choices,
+        null=False)
+
+    date = models.DateField(
+        db_column='transaction_date',
+        null=False,
+        default=datetime.now)
+    
+    description = models.TextField(
+        db_column='transaction_description',
+        null=True)
+    
+    category = models.CharField(
+        db_column='transaction_category',
+        max_length=20,
+        choices=TransactionCategory.choices,
+        null=False)
+
+    class Meta:
+        db_name='tbTransaction'
--- backend/core/user/models.py
+++ backend/core/user/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.BigAutoField(
+        db_column='user_id_pk',
+        primary_key=True)
+
+    fullname = models.CharField(
+        db_column='user_fullname',
+        max_length=255,
+        null=False)
+
+    email = models.EmailField(
+        db_column='user_email',
+        max_length=254,
+        unique=True,
+        null=False)
+
+    cpf = models.CharField(
+        db_column='user_cpf',
+        max_length=11, 
+        unique=True,
+        null=False)
+    
+    tel = models.CharField(
+        db_column='user_tel',
+        max_length=13,
+        unique=True,
+        null=False)
+
+    class Meta:
+        db_name = 'tbUser'
