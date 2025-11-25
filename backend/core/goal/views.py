9aa87fad4afca0f53e9ee63b6131181cef0d8f14
anthony-gg1 <anthonyguilhermevieiramoraes@gmail.com>
2025-11-25 09:22:25 -0500

    feat: cria as views para Goal, Transaction e User

--- backend/core/goal/views.py
+++ backend/core/goal/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Goal
+from .serializers import GoalSerializer
 
 # Create your views here.
+class GoalListCreateView(ListCreateAPIView):
+    serializer_class = GoalSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Goal.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class GoalDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = GoalSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Goal.objects.filter(user=self.request.user)
--- backend/core/transaction/views.py
+++ backend/core/transaction/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Transaction
+from .serializers import TransactionSerializer
 
 # Create your views here.
+class TransactionListCreateView(ListCreateAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TransactionDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
