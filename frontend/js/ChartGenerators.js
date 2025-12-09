145e8037e3bc9644823f60116cb30e6c83531640
PauloS5 <paulosocratesdesouza@gmail.com>
2025-12-08 23:04:36 -0500

    chore: retorna o front para uma versao mais antiga

---
+++ frontend/js/ChartGenerators.js
@@ -0,0 +1,329 @@
+﻿// Função para gerar Gráfico de Pizza para Transações
+export function generateTransactionPieChart(canvasId, revenueCount, expenseCount) {
+    const ctx = document.getElementById(canvasId);
+
+    const data = {
+        labels: ['Receitas', 'Despesas'],
+        datasets: [{            
+            label: 'Proporção entre Receitas e Despesas',
+            data: [revenueCount, expenseCount],
+            backgroundColor: ['rgb(86,255,178)', 'rgb(255,95,97)'],
+            hoverOffset: 10
+        }]
+    };
+
+    const options = {
+            responsive: true,
+            maintainAspectRatio: false,
+            borderWidth: 0,
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Receitas x Despesas",
+                    color: "#fff",
+                    font: {
+                        size: 24,
+                        weight: "bold"
+                    }
+                },
+                legend: {
+                    position: "bottom",
+                    title: {
+                        display: true,
+                        padding: 10,
+                        text: "Compara o valor total em Receita com o valor total em Despesas",
+                        color: "#fff",
+                        font: {
+                            weight: 'bolder'
+                        }
+                    },
+                    labels: {
+                        color: '#fff',
+                        padding: 10
+                    }
+                }
+            }
+        }
+
+    const chart = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: options
+    })
+}
+
+// Função para gerar Gráfico de Barras para Transações
+export function generateTransactionsBytrimesterBarChart(canvasId, valuesBytrimester) {
+    const ctx = document.getElementById(canvasId);
+
+    const data = {
+        borderColor: '#ff0000ff',
+        labels: ['1º trimestre', '2º trimestre', '3º trimestre', '4º trimestre'],
+        datasets: [
+            {
+                label: 'Total em Receitas',
+                data: valuesBytrimester.map((value) => {
+                    return value.totalRevenue;
+                }),
+                backgroundColor: '#56FFB2',
+                hoverOffset: 10
+            },
+            {
+                label: 'Total em Despesas',
+                data: valuesBytrimester.map((value) => {
+                    return value.totalExpenses;
+                }),
+                backgroundColor: '#FF5F61',
+                hoverOffset: 10
+            }
+        ]
+    };
+
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            title: {
+                display: true,
+                text: "Receitas/Despesas por trimestre",
+                color: "#fff",
+                font: {
+                    size: 24,
+                    weight: "bold"
+                }
+            },
+            legend: {
+                position: "bottom",
+                title: {
+                    display: true,
+                    padding: 10,
+                    text: "Compara a quantia de gastos e despesas por trimestre",
+                    color: "#fff",
+                    font: {
+                        weight: 'bolder'
+                    }
+                },
+                labels: {
+                    color: '#fff',
+                    padding: 10
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: '#ffffff'
+                },
+                grid: {
+                    color: '#525252'
+                }
+            },
+            y: {
+                ticks: {
+                    color: '#ffffff'
+                },
+                grid: {
+                    color: '#525252'
+                }
+            }
+        }
+    }
+
+    const chart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    })
+}
+
+// Função para gerar Gráfico de Rádio para Gategorias de Ganhos 
+export function generateRevenueCategoriesComparisionPolarAreaChart(canvasId, valuesByCategorie) {
+    const ctx = document.getElementById(canvasId);
+
+    const color = "#56FFB2";
+
+    const data = {
+        labels: ["Salário", "Investimentos", "Freelance", "Doações", "Outros"],
+        datasets: [{
+            label: 'Receita',
+            data: [
+                valuesByCategorie.salary,
+                valuesByCategorie.investments,
+                valuesByCategorie.freelance,
+                valuesByCategorie.gifts,
+                valuesByCategorie.others
+            ],
+            borderColor: color,
+            backgroundColor: color + "80",
+        }]
+    };
+
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            title: {
+                display: true,
+                text: "Lucro por Categoria",
+                color: "#fff",
+                font: {
+                    size: 24,
+                    weight: "bold"
+                }
+            },
+            legend: {
+                position: "bottom",
+                title: {
+                    display: true,
+                    padding: 10,
+                    text: "Compara a quantidade de gastos por categoria",
+                    color: "#fff",
+                    font: {
+                        weight: 'bolder'
+                    }
+                },
+                labels: {
+                    color: '#fff',
+                    padding: 10
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: '#ffffff'
+                },
+                grid: {
+                    color: '#525252'
+                }
+            },
+            y: {
+                ticks: {
+                    color: '#ffffffff'
+                },
+                grid: {
+                    color: '#525252'
+                }
+            },
+            r: {
+                ticks: {
+                    color: '#525252',
+                    font: {
+                        size: 14
+                    }
+                },
+                grid: {
+                    color: '#929292'
+                },
+                pointLabels: {
+                    color: '#fff',
+                    font: {
+                        size: 16
+                    }
+                }
+            }
+        }
+    }
+
+    const chart = new Chart(ctx, {
+        type: 'radar',
+        data: data,
+        options: options
+    })
+}
+
+// Função para gerar Gráfico de Rádio para Gategorias de Gastos 
+export function generateExpensesCategoriesComparisionPolarAreaChart(canvasId, valuesByCategorie) {
+    const ctx = document.getElementById(canvasId);
+
+    const color = "#FF5F61";
+
+    const data = {
+        labels: ["Comida", "Transporte", "Entretenimento", "Contas", "Outros"],
+        datasets: [{
+            label: 'Despesas',
+            data: [
+                valuesByCategorie.food,
+                valuesByCategorie.transportation,
+                valuesByCategorie.entertainment,
+                valuesByCategorie.utilities,
+                valuesByCategorie.others
+            ],
+            borderColor: color,
+            backgroundColor: color + "80",
+        }]
+    };
+
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            title: {
+                display: true,
+                text: "Gasto por Categoria",
+                color: "#fff",
+                font: {
+                    size: 24,
+                    weight: "bold"
+                }
+            },
+            legend: {
+                position: "bottom",
+                title: {
+                    display: true,
+                    padding: 10,
+                    text: "Compara a quantidade de gastos por categoria",
+                    color: "#fff",
+                    font: {
+                        weight: 'bolder'
+                    }
+                },
+                labels: {
+                    color: '#fff',
+                    padding: 10
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: '#ffffff'
+                },
+                grid: {
+                    color: '#525252'
+                }
+            },
+            y: {
+                ticks: {
+                    color: '#ffffffff'
+                },
+                grid: {
+                    color: '#525252'
+                }
+            },
+            r: {
+                ticks: {
+                    color: '#525252',
+                    font: {
+                        size: 14
+                    }
+                },
+                grid: {
+                    color: '#929292'
+                },
+                pointLabels: {
+                    color: '#fff',
+                    font: {
+                        size: 16
+                    }
+                }
+            }
+        }
+    }
+
+    const chart = new Chart(ctx, {
+        type: 'radar',
+        data: data,
+        options: options
+    })
+}
---
+++ frontend/js/Models.js
@@ -0,0 +1,32 @@
+﻿// Model para Usuário
+class User {
+    constructor() {
+        this.name = null;
+        this.cpf = null;
+        this.email = null;
+        this.tel = null;
+        this.profile_image = null;
+    }
+}
+
+// Model para Transação
+class Transaction {
+    constructor() {
+        this.id = null;
+        this.value = null;
+        this.description = null;
+        this.date = null;
+    }
+}
+
+// Model para Meta
+class Goal {
+    constructor() {
+        this.id = null
+        this.title = null;
+        this.description = null;
+        this.value = 1400
+        this.expiration_date = null;
+        this.status = null;
+    }
+}
